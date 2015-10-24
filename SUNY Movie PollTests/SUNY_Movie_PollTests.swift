967f4a50e507a41cc877d674a5bc2d8be1f06941
Gregory Rudolph <rudi@lolay.com>
2015-10-23 23:38:25 -0400

    Initial Commit

---
+++ SUNY Movie PollTests/SUNY_Movie_PollTests.swift
@@ -0,0 +1,36 @@
+//
+//  SUNY_Movie_PollTests.swift
+//  SUNY Movie PollTests
+//
+//  Created by Gregory Rudolph-Alverson on 10/23/15.
+//  Copyright © 2015 STEiN-Net. All rights reserved.
+//
+
+import XCTest
+@testable import SUNY_Movie_Poll
+
+class SUNY_Movie_PollTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
